5f0bb56fad83cb10c5161b81ff84e471a059d25b
Morten Jessen <mortenjessen5d@gmail.com>
2023-10-23 16:06:17 +0200

    chore: format

--- static/js/api.js
+++ static/js/api.js
@@ -1,38 +1,38 @@
 export class Api {
-  async getServices() {
-    const data = await get("/api/services");
-    const services = [];
-    for (const service of data) {
-      services.push(new Service(service.name, service.status));
+    async getServices() {
+        const data = await get("/api/services");
+        const services = [];
+        for (const service of data) {
+            services.push(new Service(service.name, service.status));
+        }
+        return services;
     }
-    return services;
-  }
 }
 
 export class Service {
-  constructor(name, status) {
-    this.name = String(name);
-    this.status = String(status);
-  }
+    constructor(name, status) {
+        this.name = String(name);
+        this.status = String(status);
+    }
 }
 
 async function get(url) {
-  return await apiFetch("GET", url);
+    return await apiFetch("GET", url);
 }
 
 async function apiFetch(method, url, payload) {
-  try {
-    const response = await fetch(url, {
-      method: method,
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
-    return await response.json();
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
+    try {
+        const response = await fetch(url, {
+            method: method,
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify(payload),
+        });
+        return await response.json();
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,31 @@
 import { Api } from "./api.js";
 
 function createPingElement(color) {
-  const ping = document.createElement("div");
-  ping.classList.add("relative", "flex", "h-3", "w-3");
-  const span1 = document.createElement("span");
-  span1.classList.add(
-    "animate-ping",
-    "absolute",
-    "inline-flex",
-    "h-full",
-    "w-full",
-    "rounded-full",
-    `bg-${color}-400`,
-    "opacity-75",
-  );
-  const span2 = document.createElement("span");
-  span2.classList.add(
-    "relative",
-    "inline-flex",
-    "rounded-full",
-    "h-3",
-    "w-3",
-    `bg-${color}-500`,
-  );
-  ping.appendChild(span1);
-  ping.appendChild(span2);
-  return ping;
+    const ping = document.createElement("div");
+    ping.classList.add("relative", "flex", "h-3", "w-3");
+    const span1 = document.createElement("span");
+    span1.classList.add(
+        "animate-ping",
+        "absolute",
+        "inline-flex",
+        "h-full",
+        "w-full",
+        "rounded-full",
+        `bg-${color}-400`,
+        "opacity-75",
+    );
+    const span2 = document.createElement("span");
+    span2.classList.add(
+        "relative",
+        "inline-flex",
+        "rounded-full",
+        "h-3",
+        "w-3",
+        `bg-${color}-500`,
+    );
+    ping.appendChild(span1);
+    ping.appendChild(span2);
+    return ping;
 }
 
 /**
@@ -33,42 +33,42 @@ function createPingElement(color) {
  * @returns {HTMLElement}
  */
 function createServiceElement(service) {
-  const name = document.createElement("h2");
-  name.innerText = service.name;
-  const pingColor = {
-    Healthy: "green",
-    Unhealthy: "red",
-  };
-  const ping = createPingElement(pingColor[service.status]);
+    const name = document.createElement("h2");
+    name.innerText = service.name;
+    const pingColor = {
+        Healthy: "green",
+        Unhealthy: "red",
+    };
+    const ping = createPingElement(pingColor[service.status]);
 
-  const container = document.createElement("div");
-  container.classList.add(
-    "flex",
-    "flex-col",
-    "justify-center",
-    "items-center",
-    "rounded-full",
-    "border",
-    "border-gray-400",
-    "h-24",
-    "w-24",
-    "m-4",
-  );
-  container.appendChild(ping);
-  container.appendChild(name);
-  return container;
+    const container = document.createElement("div");
+    container.classList.add(
+        "flex",
+        "flex-col",
+        "justify-center",
+        "items-center",
+        "rounded-full",
+        "border",
+        "border-gray-400",
+        "h-24",
+        "w-24",
+        "m-4",
+    );
+    container.appendChild(ping);
+    container.appendChild(name);
+    return container;
 }
 
 async function main() {
-  const main = document.querySelector("main");
-  main.classList.add("flex", "flex-wrap", "justify-center", "items-center");
-  const api = new Api();
-  const services = await api.getServices();
+    const main = document.querySelector("main");
+    main.classList.add("flex", "flex-wrap", "justify-center", "items-center");
+    const api = new Api();
+    const services = await api.getServices();
 
-  main.innerHTML = "";
-  for (const service of services) {
-    main.appendChild(createServiceElement(service));
-  }
+    main.innerHTML = "";
+    for (const service of services) {
+        main.appendChild(createServiceElement(service));
+    }
 }
 
 setInterval(main, 3_000);
